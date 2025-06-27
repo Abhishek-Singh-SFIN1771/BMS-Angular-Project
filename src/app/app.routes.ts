bd52c897a1014f61732a8023c1e2c03cc481c6e9
Abhishek Singh <singhabhishek696932@gmail.com>
2025-06-27 13:00:20 +0530

    updates due to filne name changed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
-import { BookformComponent } from './BookForms/book-form.component';
-import { BooklistComponent } from './BookLists/book-list.component';
+import { BookformComponent } from './book-forms/book-form.component';
+import { BooklistComponent } from './book-lists/book-list.component';
+
 
 export const routes: Routes = 
 [
